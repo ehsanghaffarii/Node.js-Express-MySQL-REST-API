7e69daf4bb58c78c5a54842c4910e88dce90e7b5
Ehsanghaffarii <ghafari.5000@gmail.com>
2020-06-02 02:16:40 +0430

    auth & api-key

--- index.js
+++ index.js
@@ -2,12 +2,38 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { pool } = require('./config')
+const helmet = require('helmet')
+const compression = require('compression')
+const rateLimit = require('express-rate-limit')
+const { body, check } = require('express-validator')
 
 const app = express();
 
+// app.use 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(compression())
+app.use(helmet())
+app.use(cors(origin))
+app.use(limiter)
+
+
+const isProduction = process.env.NODE_ENV === 'production'
+const origin = {
+    origin: isProduction ? 'https://www.example.com' : '*',
+}
+
+// limiter for every endpoint
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 5, // 5 requests
+})
+
+// limiter for post endpoint
+const postLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 1, // 1 request
+})
 
 // get books request
 const getBooks = (request, response) => {
@@ -31,15 +57,52 @@ const addBook = (request, response) => {
     })
 }
 
+// delete book request
+const deleteBook = (request, response) => {
+    if (!request.header('apiKey') || request.header('apiKey') !== process.env.API_KEY) {
+        return response.status(401).json({ status: 'error', message: 'Unauthorized'})
+    }
+}
+
 app
     .route('/books')
     // GET endpoint
     .get(getBooks)
     // POST endpoint
-    .post(addBook)
+    .post('/books',
+    [
+        check('author')
+            .not()
+            .isEmpty()
+            .isLength({ min: 5, max: 255 })
+            .trim(),
+        check('title')
+            .not()
+            .isEmpty()
+            .isLength({ min: 5, max: 255 })
+            .trim(),
+    ],
+    postLimiter,
+    (request, response) => {
+        const errors = validationResult(request)
+
+        if ( !errors.isEmpty()) {
+            return response.status(422).json({ errors: errors.array() })
+        }
+
+        const { author, title} = request.body
+
+        pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+            if (error) {
+                throw error
+            }
+            response.status(201).json({ status: 'success', message: 'Book added' })
+        })
+    },
+    addBook)
+
 
 // Start server
-
 app.listen(process.env.PORT || 3002, () => {
     console.log('server Listening')
 })
