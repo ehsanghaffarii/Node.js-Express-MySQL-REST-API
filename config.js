05542c716f7e0ee639a1683417b7173e239c57a0
Ehsanghaffarii <ghafari.5000@gmail.com>
2020-06-02 01:00:51 +0430

    heroku

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 
 require('dotenv').config();
 
-const mysql = require('mysql');
+const { Pool } = require('pg');
 
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 
-const connectionString = 'mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}'
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 
-const pool = new mysql.createPool({
+const pool = new Pool({
     connectionString: isProduction ?
     process.env.DATABASE_URL : connectionString,
     ssl: isProduction,
