f57a56f7abcc7dd0b59d5663d3ff179efaa82279
Ehsanghaffarii <ghafari.5000@gmail.com>
2020-06-01 11:14:09 +0430

    update codes

--- config.js
+++ config.js
@@ -1,12 +1,16 @@
 
 require('dotenv').config();
 
-const { Pool } = require('mysql');
+const mysql = require('mysql');
+
+
 const isProduction = process.env.NODE_ENV === 'production';
 
-const connectionString = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.PORT}/${process.env.DB_DATABASE}`
 
-const pool = new Pool({
+const connectionString = 'mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}'
+
+
+const pool = new mysql.createPool({
     connectionString: isProduction ?
     process.env.DATABASE_URL : connectionString,
     ssl: isProduction,
